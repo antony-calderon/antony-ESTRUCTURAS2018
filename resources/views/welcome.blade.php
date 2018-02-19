0c5dab72012a814f63c69e41b48ffe78f51b4411
Unknown <alex_aact@hotmail.com>
2018-02-19 09:36:57 -0500

    Primer Proyecto
    iniciando en la web

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,242 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+
+
+
+
+
+
+
+
+
+
+                    //Laravel<!doctype <!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8" />
+    <title>estructuras de informacion </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+<body bgcolor="#212F3D">
+        <h1 align="center"><font color="white"><marquee>ESTRUCTURAS DE INFORMACIÓN</marquee></font></h1>
+        <hr>
+        <h2><font color="#BDC3C7">ESTRUCTURA DE DATOS</font></h2>
+        
+        <p><font color="white"><pre>
+            ▸ Son aquellas que nos permiten almacenar, manipular y ordenar los datos.
+            ▸ Son una colección de datos cuya organización se caracteriza por las
+            funciones de acceso que se usan para almacenar y acceder elementos
+            Individuales de datos.
+            ▸ Es una forma de como están organizado un conjunto de datos y como se
+            encuentran interconectados.
+            ▸ Estas estructuras solo existen en la memoria ram, cierras el programa o
+            liberas memoria y desaparecen todos los datos.
+            ▸ Función principal es buscar la eficiencia tanto en memoria como tiempos
+            de ejecución.
+         </p></font></pre>
+
+         <h2><font color="#BDC3C7">IMPORTANCIA</font></h2>
+
+         <P align="center"><font color="white"><pre>
+            la mayoria de las estructuras de datos 
+            estan inspiradas en la vida real y 
+            usa los mismos terminos la maoria
+            de las veces.
+        </pre></font></P>
+
+        <h2><font color="#BDC3C7">OPERACIONES</font></h2>
+
+        <P align="right"><font color="white"><PRE>
+            ▸ Recorrido : Implica el acceder a cada registro una única vez aunque uno o más ítems
+              del registro sean procesados.
+            ▸ Búsqueda: Implica la localización de un registro caracterizado por una determinada
+              clave o también el acceso a todos los registros que cumplan una o más condiciones.
+            ▸ Inserción: Cuando añadimos nuevos registros a la estructura.
+            ▸ Eliminación: Operación de borrado de un registro de la estructura.
+            ▸ Ordenación: Es la operación de clasificar los registros conforme a un orden lógico
+              determinado (por ejemplo, alfabéticamente, de acuerdo a una clave de nombre, o
+              numérica, de acuerdo a alguna clave de número).
+            ▸ Mezcla: Es la operación de combinar dos archivos previamente ordenados en uno
+              único que también lo está.
+        </PRE></font></P>
+
+        <H2><font color="#BDC3C7">TIPOS DE ESTRUCTURAS</font></H2>
+
+        <P><font color="white"><PRE>
+                ▸ Primitivas
+
+                 ▸ Son primitivas aquellas que no están compuestas por otras
+                   estructuras de datos por ejemplo, enteros, booleanos y
+                   caracteres.
+
+                ▸ No Primitivas
+
+                 ▸ Estructuras de datos se pueden construir de una o mas
+                   primitivas. Las estructuras de datos simples que
+                   consideramos se construyen a partir de estructuras primitivas
+                   y son: cadenas, arreglos y registros.
+        </PRE></font></P>
+
+        <H2><font color="#BDC3C7">LINEALES Y NO LINEALES</font></H2>
+
+        <P><font color="white"><PRE> 
+            Lineales:
+
+                ▸ Derivados del concepto de secuencia (secuencias es un conjuntos de elementos entre
+                  los que se establece una relación de predecesor y sucesor).
+                ▸ Desde el punto de vista de la informática, existen tres estructuras lineales
+                  especialmente importantes: las pilas, las colas y las listas.
+                ▸ Las operaciones básicas para dichas estructuras son:
+                   • crear la secuencia vacía
+                   • añadir un elemento a la secuencia
+                   • borrar un elemento a la secuencia
+                   • consultar un elemento de la secuencia
+                   • comprobar si la secuencia está vacía
+
+                ▸ Definición
+
+                 • Pilas: las tres operaciones actúan sobre el final de la secuencia
+                 • Colas: se añade por el final y se borra y consulta por el principio
+                 • Listas: las tres operaciones se realizan sobre una posición privilegiada de la secuencia, la cual puede
+                   desplazarse.
+
+            No Lineales:
+
+                   ▸ Estructuras de datos multienlazadas. Cada elemento puede estar enlazado a
+                     cualquier otro componentes.
+                   ▸ En estas estructuras cada elemento puede tener varios elementos “siguientes”, lo
+                     cual introduce el concepto de estructuras de ramificación.
+                   ▸ Estas estructuras de datos de ramificación son llamadas grafos y arboles.
+                   ▸ Un grafo es un conjunto de puntos y un conjunto de líneas, con cada línea se
+                     une un punto a otro.
+                   ▸ Un árbol es un grafo conexo, simple y aciclico. Un árbol no contiene ni ciclos
+                     ni bucles; existe una sola arista entre cualquier par de nodos.
+        </PRE></font></P>
+
+        <H2><font color="#BDC3C7">PUEDEN SER CLASIFICADAS EN:</font></H2>
+        <H3><font color="#BDC3C7">ESTATICAS Y DINAMICAS</font></H3>
+
+        <P><font color="white"><PRE>
+            ▸ Estáticas
+
+                   ▸ Son aquellas en las que se asigna una cantidad fija de
+                     memoria cuando la variable se declara. (en tiempo de
+                     compilación).
+            ▸ Dinámicas
+
+                   ▸ Son aquellas cuya ocupación en memoria pueden
+                     aumentar o disminuir en tiempo de ejecución.(en
+                     tiempo de ejecución)..
+                    </PRE></font></P>
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
